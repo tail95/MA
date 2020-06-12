ed62f6b28402756a5ce21b336cecec8a8bccfc11
tail95 <xodlf8637@naver.com>
2020-06-12 20:36:47 +0900

    update

--- Utils.py
+++ Utils.py
@@ -1,5 +1,39 @@
 import cv2
 import pyautogui
+import time
 
 def secondaryLogin():
-    pyautogui.locateCenterOnScreen()
+    tmp = pyautogui.locateCenterOnScreen("resources/w.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/l.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/d.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/m.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/s.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/1.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/5.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+    tmp = pyautogui.locateCenterOnScreen("resources/2.png")
+    pyautogui.moveTo(tmp[0], tmp[1], 0.1)
+    pyautogui.click()
+
+
+secondaryLogin()
